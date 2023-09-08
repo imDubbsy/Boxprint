dccf407ac329b61dc0b5406259168dbd911c5a6c
imDubbsy <michaeljzarrilli@gmail.com>
2023-09-07 23:11:52 -0400

    First commit

---
+++ boxprint.py
@@ -0,0 +1,24 @@
+"""
+
+************
+*          *
+*          *
+*          *
+************
+
+"""
+
+def boxPrint(symbol, width, height):
+    if len(symbol) != 1:
+        raise Exception('"symbol" needs to be a string of length 1.')
+    if (width < 2) or (height < 2):
+        raise Exception('"width and "height" must be greater or equal to 2.')
+
+    for i in range(height - 2):
+        print(symbol + (' ' * (width - 2)) + symbol)
+        
+    print(symbol * width)
+
+boxPrint('*', 2, 2)
+
+    
